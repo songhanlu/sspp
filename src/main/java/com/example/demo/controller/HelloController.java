c20832c8931903961dba109d53c2e5758578ea6c
songhanlu <1722382969@qq.com>
2018-02-05 11:09:04 +0800

    yyy

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hp on 2018/2/5.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
